12b8a591f52caab54db38200e7488c6c4fdb49ba
Ivan <inzarubin80@gmail.com>
2020-02-04 22:03:30 +0300

    ,,

--- src/components/todo/Constants.js
+++ src/components/todo/Constants.js
@@ -1,2 +1,2 @@
-export const API_URL = 'https://i-todo.herokuapp.com'
-export const JPA_API_URL = 'https://i-todo.herokuapp.com/jpa'
+export const API_URL = 'https://restful-web-services-todo.herokuapp.com/'
+export const JPA_API_URL = 'https://restful-web-services-todo.herokuapp.com/jpa'
